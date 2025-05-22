b6cc90c90b2bf2873068f035cb62c3640989eced
Helg <Helg>
2025-05-22 16:48:16 +0300

    +

---
+++ OpenLayersTest/main.js
@@ -0,0 +1,29 @@
+import Map from './node_modules/ol/Map.js';
+import View from './node_modules/ol/View.js';
+import TileLayer from './node_modules/ol/layer/Tile.js';
+import OSM from './node_modules/ol/source/OSM.js';
+
+const map = new Map({
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
+  ],
+  target: 'map',
+  view: new View({
+    center: [0, 0],
+    zoom: 2
+  }),
+});
+
+document.getElementById('zoom-out').onclick = function () {
+  const view = map.getView();
+  const zoom = view.getZoom();
+  view.setZoom(zoom - 1);
+};
+
+document.getElementById('zoom-in').onclick = function () {
+  const view = map.getView();
+  const zoom = view.getZoom();
+  view.setZoom(zoom + 1);
+};
