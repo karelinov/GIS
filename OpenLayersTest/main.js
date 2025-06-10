36fe8045302ef2635f7368c1faaba7daca9133f0
Helg <Helg>
2025-06-10 13:01:40 +0300

    +

--- OpenLayersTest/main.js
+++ OpenLayersTest/main.js
@@ -1,54 +1,37 @@
-import Map from './node_modules/ol/Map.js';
-import View from './node_modules/ol/View.js';
-import TileLayer from './node_modules/ol/layer/Tile.js';
-import OSM from './node_modules/ol/source/OSM.js';
-import VectorTileLayer from './node_modules/ol/layer/VectorTile.js';
-import MVT from './node_modules/ol/format/MVT.js';
-import VectorTileSource from './node_modules/ol/source/VectorTile.js';
-import {fromLonLat} from './node_modules/ol/proj.js';
-import {applyStyle} from './node_modules/ol-mapbox-style/src/index.js';
-import {applyBackground} from './node_modules/ol-mapbox-style/src/index.js';
-import Fill from 'ol/style/Fill.js';
-import Icon from 'ol/style/Icon.js';
-import Stroke from 'ol/style/Stroke.js';
-import Style from 'ol/style/Style.js';
-import Text from 'ol/style/Text.js';
+import {Map} from 'ol';
+import {View} from 'ol';
+import {Feature} from 'ol';
+import * as olProj from 'ol/proj';
 
-const startLonLat = [37.689134,55.951649];
-const startMercator = fromLonLat(startLonLat);
+const configJSON = await fetch('../../config/config.json').then(response => response.json());
 
-const style = await fetch('http://localhost:8081/styles/maptiler-basic/style.json').then(response => response.text());
-
-var layer = new VectorTileLayer({
-      source: new VectorTileSource({
-        format: new MVT(),
-        url: 'http://localhost:8081/data/openmaptiles/{z}/{x}/{y}.pbf',
-      }),
-      //style: createMapboxStreetsV6Style(Style, Fill, Stroke, Icon, Text)
-      //style: style
-
-    });
-applyStyle(layer, 'http://localhost:8081/styles/maptiler-basic/style.json');
-//applyStyle(layer, 'mapbox://styles/mapbox/bright-v9');
-
-var layer1 = new TileLayer({
-      source: new OSM({
-        url: 'http://localhost:8081/styles/test-style/512/{z}/{x}/{y}.png',
-      })
-    });
-
+import {layer as tileLayer_PNG} from './Layers/TileLayer_PNG/Layer.js';
+import {layer as tileLayer_PBF} from './Layers/TileLayer_PBF/Layer.js';
+import {layer as ObjectLayer_Main} from './Layers/ObjectLayer_Main/Layer.js';
+import {layer as ObjectLayer_Selection} from './Layers/ObjectLayer_Selection/Layer.js';
+import {onPointerMove} from './Layers/ObjectLayer_Selection/Layer.js';
 
 const map = new Map({
   target: 'map',
   view: new View({
     // 37.689134,55.951649,7
-    center: startMercator,
+    center: olProj.fromLonLat(configJSON.startLonLat),
     zoom: 7,
   }),
   layers: [
-    layer1,
+    tileLayer_PNG,
+    tileLayer_PBF,
+    ObjectLayer_Main,
+    ObjectLayer_Selection
   ],
 });
-//var layer = map.getLayers().array_[0];
-//applyStyle(layer, 'http://localhost:8081/styles/maptiler-basic/style.json', {accessToken: 'YOUR_MAPBOX_TOKEN'});
-//applyBackground(layer, 'http://localhost:8081/styles/maptiler-basic/style.json', {accessToken: 'YOUR_MAPBOX_TOKEN'});
+
+map.on('pointermove', function (event) {
+  ObjectLayer_Selection.getSource().clear();
+  //! [get-features]
+  const features = map.getFeaturesAtPixel(event.pixel, {
+    layerFilter: (layer) => layer === ObjectLayer_Main,
+  });
+  onPointerMove(event, features);
+});
+
